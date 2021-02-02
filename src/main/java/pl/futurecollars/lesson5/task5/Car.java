6b2e23926e7bc6f39e1e27d17abad3698eefd4e1
Paweł Kazanecki <kazan87@gmail.com>
2021-02-03 00:18:35 +0100

    Created interface for motor vehicles, all methods for motor vehicle implemented in car class, deleted unnecessary methods form bicycle class

--- src/main/java/pl/futurecollars/lesson5/task5/Car.java
+++ src/main/java/pl/futurecollars/lesson5/task5/Car.java
@@ -1,5 +1,27 @@
 package pl.futurecollars.lesson5.task5;
 
-public class Car extends Vehicle {
+public class Car extends Vehicle implements MotorVehicle {
+
+    private boolean engineStarted;
+
+    public void startEngine() {
+        if (!engineStarted) {
+            engineStarted = true;
+        }
+    }
+
+    public void stopEngine() {
+        if (engineStarted) {
+            engineStarted = false;
+        }
+    }
+
+    public boolean isEngineStarted() {
+        return engineStarted;
+    }
+
+    public void setEngineStarted(boolean engineStarted) {
+        this.engineStarted = engineStarted;
+    }
 
 }
---
+++ src/main/java/pl/futurecollars/lesson5/task5/MotorVehicle.java
@@ -0,0 +1,13 @@
+package pl.futurecollars.lesson5.task5;
+
+public interface MotorVehicle {
+
+    void startEngine();
+
+    void stopEngine();
+
+    boolean isEngineStarted();
+
+    void setEngineStarted(boolean engineStarted);
+}
+
